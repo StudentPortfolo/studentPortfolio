8831be59de2c53aa66d68f615c128ccc6393e5a9
Pavan Kumar R <128024007+Pavan-Kumar97@users.noreply.github.com>
2023-03-16 17:41:38 +0530

    Pa1: Navbar, Home

--- student-portfolio/src/App.js
+++ student-portfolio/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import HomeHeader from "../src/components/Header/HomeHeader";
+import HomeFooter from "../src/components/Footer/HomeFooter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HomeHeader />
       </header>
+      <footer className="App-footer">
+        <HomeFooter />
+      </footer>
     </div>
   );
 }
---
+++ student-portfolio/src/components/Footer/HomeFooter.js
@@ -0,0 +1,9 @@
+const HomeFooter = () => {
+  return (
+    <div>
+      <h1>Home footer</h1>
+    </div>
+  );
+};
+
+export default HomeFooter;
